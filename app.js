fc50354a8faab10ad55c414682e1a0eb18723eab
Rehan <rehankhan6711@gmail.com>
2020-09-06 09:06:13 +0530

    bilal ki maa ka bhosda

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
  const navSlide = () => {
      const burger = document.querySelector('.burger');
      const nav = document.querySelector('.menu');
+     const prop1 = document.querySelector('.prop-in-menu-1')
+     const prop2 = document.querySelector('.prop-in-menu-2')
      const navLinks = document.querySelectorAll('.menu li');
+
      burger.addEventListener('click', () => {
          nav.classList.toggle('nav-active');
+         prop1.classList.toggle('prop-width');
+         prop2.classList.toggle('prop-width');
          navLinks.forEach((link, index) => {
              if (link.style.animation) {
                  link.style.animation = '';
